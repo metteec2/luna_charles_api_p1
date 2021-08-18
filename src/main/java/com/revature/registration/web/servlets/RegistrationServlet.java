3cda8131b927af26f1b1b980d865f271898fa837
LunaHaines <nhaines95@gmail.com>
2021-08-18 09:05:37 -0600

    added endpoints/servlets for course, registration, student, and faculty

---
+++ src/main/java/com/revature/registration/web/servlets/RegistrationServlet.java
@@ -0,0 +1,16 @@
+package com.revature.registration.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getMethod() + " received from client.");
+        resp.getWriter().write("<h1>Register for Courses</h1>");
+    }
+}
