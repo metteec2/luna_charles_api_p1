b278ee3325096f80776e4e1f7e25d49e4400dfc6
LunaHaines <nhaines95@gmail.com>
2021-08-20 07:49:31 -0600

    added a cors filter

---
+++ src/main/java/com/revature/registration/web/filters/CorsFilter.java
@@ -0,0 +1,20 @@
+package com.revature.registration.web.filters;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) throws ServletException, IOException {
+        req.setAttribute("filtered",true);
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        filterChain.doFilter(req, resp);
+    }
+}
