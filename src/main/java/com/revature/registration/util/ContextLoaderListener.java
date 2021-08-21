cf454d5ec002dee778b5b15ce7eb7d5eb0192be5
metteec2 <chmettee@gmail.com>
2021-08-21 11:33:32 -0400

    Added fields and constructor to RegistrationServlet and updated ContextLoaderListener to accept correct parameters

--- src/main/java/com/revature/registration/util/ContextLoaderListener.java
+++ src/main/java/com/revature/registration/util/ContextLoaderListener.java
@@ -2,7 +2,6 @@ package com.revature.registration.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
 import com.revature.registration.repositories.CourseRepository;
 import com.revature.registration.repositories.FacultyRepository;
 import com.revature.registration.repositories.StudentRepository;
@@ -32,7 +31,7 @@ public class ContextLoaderListener implements ServletContextListener {
         WelcomeServlet welcomeServlet = new WelcomeServlet(objectMapper);
         HealthCheckServlet healthCheckServlet = new HealthCheckServlet();
         CourseServlet courseServlet = new CourseServlet(courseServices, userServices, objectMapper);
-        RegistrationServlet registrationServlet = new RegistrationServlet();
+        RegistrationServlet registrationServlet = new RegistrationServlet(userServices, objectMapper);
         StudentServlet studentServlet = new StudentServlet(userServices,courseServices,objectMapper);
         FacultyServlet facultyServlet = new FacultyServlet(userServices,objectMapper);
         AuthStudentServlet authStudentServlet = new AuthStudentServlet(userServices,objectMapper);
--- src/main/java/com/revature/registration/web/servlets/RegistrationServlet.java
+++ src/main/java/com/revature/registration/web/servlets/RegistrationServlet.java
@@ -1,16 +1,40 @@
 package com.revature.registration.web.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.registration.services.UserServices;
+import com.revature.registration.web.dtos.ErrorResponse;
+import com.revature.registration.web.dtos.Principal;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RegistrationServlet extends HttpServlet {
+
+    private final UserServices userServices;
+    private final ObjectMapper objectMapper;
+
+    public RegistrationServlet(UserServices userServices, ObjectMapper objectMapper) {
+        this.userServices = userServices;
+        this.objectMapper = objectMapper;
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println(req.getMethod() + " received from client.");
         resp.getWriter().write("<h1>Register for Courses</h1>");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter respWriter = resp.getWriter();
+        resp.setContentType("application/json");
+
     }
 }
