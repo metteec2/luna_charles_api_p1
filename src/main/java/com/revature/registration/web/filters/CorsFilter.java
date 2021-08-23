aa7c987c65d679dbb349935679378c35502ed99b
metteec2 <chmettee@gmail.com>
2021-08-23 12:32:05 -0400

    edited corsfilter

--- src/main/java/com/revature/registration/web/filters/CorsFilter.java
+++ src/main/java/com/revature/registration/web/filters/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter extends HttpFilter {
         req.setAttribute("filtered",true);
         resp.setHeader("Access-Control-Allow-Origin", "*");
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        resp.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(req, resp);
     }
 }
